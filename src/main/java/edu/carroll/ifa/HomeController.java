4c662a4b50fb3f4ec29282c85d262f0bfed5585f
David Macy <dmacy@carroll.edu>
2023-08-29 09:44:21 -0600

    dynamic html

--- src/main/java/edu/carroll/ifa/HomeController.java
+++ src/main/java/edu/carroll/ifa/HomeController.java
@@ -1,12 +1,16 @@
 package edu.carroll.ifa;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
-    @GetMapping("/home")
-    public String index() {
-        return "Welcome to IFA!";
+    @GetMapping("/")
+    public String index(@RequestParam(value="name", required=false, defaultValue="Student")String name, Model model) {
+        model.addAttribute("name", name);
+        return "index";
     }
 }
