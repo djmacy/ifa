7552b12877b5dbc2d60f58930cd06663ce412da2
David Macy <dmacy@carroll.edu>
2023-09-06 07:46:02 -0600

    setting up MySQL

--- src/main/java/edu/carroll/ifa/web/controller/RegisterController.java
+++ src/main/java/edu/carroll/ifa/web/controller/RegisterController.java
@@ -1,12 +1,20 @@
 package edu.carroll.ifa.web.controller;
 
+import edu.carroll.ifa.web.form.LoginForm;
 import edu.carroll.ifa.web.form.RegisterForm;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class RegisterController {
+
     @GetMapping("/register")
     public String registerGet(Model model) {
         model.addAttribute("registerForm", new RegisterForm());
