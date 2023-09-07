b57dd4c20d32328e17090efccab5777b234bc0ec
David Macy <dmacy@carroll.edu>
2023-09-07 16:59:42 -0600

    added another user and it works

--- src/main/java/Pass.java
+++ src/main/java/Pass.java
@@ -1,7 +1,9 @@
 public class Pass {
     public static void main(String[] args) {
         final String hash = Integer.toString("1234".hashCode());
-        System.out.println(hash);
+        final String hash1 = Integer.toString("password".hashCode());
+        System.out.println(hash1);
         //prints 1509442
+        //hash2 prints 1216985755
     }
 }
