42feb55aaa636773569dbbb6640f72575860bb74
Ryan <rovalley@carroll.edu>
2023-09-25 11:48:06 -0600

    edit plus add file ignore

--- src/main/java/edu/carroll/ifa/web/form/LoginForm.java
+++ src/main/java/edu/carroll/ifa/web/form/LoginForm.java
@@ -1,12 +1,15 @@
 package edu.carroll.ifa.web.form;
 
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public class LoginForm {
     @NotNull
+    @Size(min = 6, message = "Username must be at least 6 characters long")
     private String username;
     @NotNull
+    @Size(min = 8, message = "Password must be at least 8 characters long")
     private String password;
 
     public String getUsername() {
@@ -24,4 +27,5 @@ public class LoginForm {
     public void setPassword(String password) {
         this.password = password;
     }
+
 }
