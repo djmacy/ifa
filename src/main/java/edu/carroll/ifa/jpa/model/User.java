be1cc5d753aba90b01bcc3b97aee2d8c3be56fd1
David Macy <dmacy@carroll.edu>
2023-09-28 07:48:40 -0600

    javaDoc for User class

--- src/main/java/edu/carroll/ifa/jpa/model/User.java
+++ src/main/java/edu/carroll/ifa/jpa/model/User.java
@@ -25,8 +25,20 @@ public class User {
     @Column(name = "password", nullable = false)
     private String hashedPassword;
 
+    /**
+     * No-argument constructor for Spring Dat JPA.
+     */
     public User() {
     }
+
+    /**
+     * Constructor for the user.
+     * @param username associated username of the user
+     * @param hashedPassword associated hashed password of the user
+     * @param firstName associated first name of the user
+     * @param lastName associated last name of the user
+     * @param age associated age of the user
+     */
     public User(String username, String hashedPassword, String firstName, String lastName, Integer age) {
         this.username = username;
         this.hashedPassword = hashedPassword;
@@ -35,50 +47,99 @@ public class User {
         this.age = age;
     }
 
+    /**
+     * Returns the user's id.
+     * @return user id
+     */
     public Integer getId() {
         return id;
     }
 
+    /**
+     * Returns the user's username.
+     * @return user username
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     * Sets the user's username.
+     * @param username associated username for the user
+     */
     public void setUsername(String username) {
         this.username = username;
     }
 
+    /**
+     * Returns the user's first name.
+     * @return user first name
+     */
     public String getFirstName() {
         return firstName;
     }
 
+    /**
+     * Sets the user's first name.
+     * @param firstName associated first name for the user
+     */
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
 
+    /**
+     * Returns the user's last name.
+     * @return user last name
+     */
     public String getLastName() {
         return lastName;
     }
 
+    /**
+     * Sets the user's last name.
+     * @param lastName associated last name for the user
+     */
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
 
+    /**
+     * Returns the user's age.
+     * @return user age
+     */
     public Integer getAge() {
         return age;
     }
 
+    /**
+     * Sets the user's age.
+     * @param age associated age for the user
+     */
     public void setAge(Integer age) {
         this.age = age;
     }
 
+    /**
+     * Returns the user's hashed password.
+     * @return associated hashed password for the user
+     */
     public String getHashedPassword() {
         return hashedPassword;
     }
 
+    /**
+     * Sets the user's hashed password
+     * @param hashedPassword associated hashed password for the user
+     */
     public void setHashedPassword(String hashedPassword) {
         this.hashedPassword = hashedPassword;
     }
 
+    /**
+     * Compare two User objects for equality.
+     * @param o the object that gets compared with the user
+     * @return true if the objects are equal based on username and hashed password. False otherwise.
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o)
@@ -90,6 +151,10 @@ public class User {
         return username.equals(user.username) && hashedPassword.equals(user.hashedPassword);
     }
 
+    /**
+     * Returns a hash code value for the User object.
+     * @return a hash code value for the User
+     */
     @Override
     public int hashCode() {
         return Objects.hash(username, hashedPassword);
