90fe2512bc6099b8cb7eee4a2f1e8bb22df747c1
David Macy <dmacy@carroll.edu>
2023-09-28 10:00:10 -0600

    javaDoc on LoginController

--- src/main/java/edu/carroll/ifa/web/controller/LoginController.java
+++ src/main/java/edu/carroll/ifa/web/controller/LoginController.java
@@ -18,15 +18,33 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 public class LoginController {
    private final UserService userService;
 
+    /**
+     * Constructs a LoginController instance with the UserService dependency.
+     * @param userService - UserService implementation used in the LoginController
+     */
    public LoginController(UserService userService) {
        this.userService = userService;
    }
+
+    /**
+     * Handles the GET request for the /login page. It also initializes the login form model and displays the page.
+     * @param model - Model object for storing attributes associated with logging in
+     * @return login page
+     */
     @GetMapping("/login")
     public String loginGet(Model model) {
         model.addAttribute("loginForm", new LoginForm());
         return "login";
     }
 
+    /**
+     * Handles the POST request for the /login page. It also authenticates the user to make they have permission to be on the page.
+     * @param loginForm - LoginForm information that is submitted by the user
+     * @param result - BindingResult for validating the form information
+     * @param session - HttpSession for managing session data
+     * @param attrs - RedirectAttributes for passing attributes to the /loginSuccess page
+     * @return login page if user has incorrect information, otherwise the loginSuccess page
+     */
     @PostMapping("/login")
     public String loginPost(@Valid @ModelAttribute LoginForm loginForm, BindingResult result, HttpSession session, RedirectAttributes attrs) {
         if (result.hasErrors()) {
@@ -42,6 +60,13 @@ public class LoginController {
         return "redirect:/loginSuccess";
     }
 
+    /**
+     * Handles the GET request for the /loginSuccess page as well as display the login success page or redirecting them to
+     * the login page if they have not logged in yet.
+     * @param session - HttpSession for managing session data
+     * @param model - Model for storing the attributes of the user
+     * @return loginSuccess page if the user has been authenticated, otherwise login page
+     */
     @GetMapping("/loginSuccess")
     public String loginSuccess(HttpSession session, Model model) {
        String sessionUsername = (String) session.getAttribute("username");
@@ -52,10 +77,5 @@ public class LoginController {
        } else {
            return "redirect:/login";
        }
-    }
-
-    @GetMapping("/loginFailure")
-    public String loginFailure() {
-        return "loginFailure";
     }
 }
